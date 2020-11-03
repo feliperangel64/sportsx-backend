fa08cb948ab389be6fb2f1dc3d39ed1dfd71cc53
Felipe Rangel <felipe.rangel.64@gmail.com>
2020-11-03 11:08:36 -0300

    refatoracao controller e model

--- src/controllers/ClienteController.js
+++ src/controllers/ClienteController.js
@@ -1,20 +1,27 @@
 const config = require('../configs/dbConfig')
 const sql = require('mssql')
+
 const Clientes = require('../models/Clientes')
 
 const findAll = async (req, res) => {
-  let pool = await sql.connect(config)
-  let clientes = await new Clientes(pool).findAll()
-  return res.json(clientes)
+  try {
+    let pool = await sql.connect(config)
+    let clientes = await new Clientes(pool).findAll()
+    return res.status(200).json(clientes)
+  } catch (error) {
+    console.log(error)
+    return res.status(500).send('Não foi possível buscar os clientes.')
+  }
 }
 
 const findById = async (req, res) => {
   try {
     let pool = await sql.connect(config)
     let cliente = await new Clientes(pool).findById(sql.Int, req.params.id)
-    return res.json(cliente)
+    return res.status(200).json(cliente)
   } catch (error) {
     console.log(error)
+    return res.status(500).send('Não foi possível encontrar o cliente.')
   }
 }
 
@@ -22,88 +29,44 @@ const findByName = async (req, res) => {
   try {
     let pool = await sql.connect(config)
     let cliente = await new Clientes(pool).findByName(req.params.name)
-    return res.json(cliente)
+    return res.status(200).json(cliente)
   } catch (error) {
     console.log(error)
+    return res.status(500).send('Não foi possível encontrar o cliente.')
   }
 }
 
 const remove = async (req, res) => {
   try {
     let pool = await sql.connect(config)
-    let cliente = await new Clientes(pool).remove(sql.Int, req.params.id)
-    return res.json(cliente)
+    let response = await new Clientes(pool).remove(sql.Int, req.params.id)
+    return res.status(204).json(response)
   } catch (error) {
     console.log(error)
+    return res.status(500).send('Não foi possível deletar o cliente.')
   }
 }
 
 const create = async (req, res) => {
-  const {
-    TipoPessoa,
-    Cpf,
-    Cnpj,
-    NomeCliente,
-    RazaoSocial,
-    Cep,
-    Email,
-    Classificacao,
-    TelefoneResidencial,
-    TelefoneComercial,
-  } = req.body
   try {
     let pool = await sql.connect(config)
-    let cliente = await new Clientes(pool).create(
-      TipoPessoa,
-      Cpf,
-      Cnpj,
-      NomeCliente,
-      RazaoSocial,
-      Cep,
-      Email,
-      Classificacao,
-      TelefoneResidencial,
-      TelefoneComercial,
-    )
-    res.status(201).json(cliente)
-  } catch (err) {
-    console.log(err)
+    let response = await new Clientes(pool).create(req.body)
+    return res.status(201).json(response)
+  } catch (error) {
+    console.log(error)
+    return res.status(500).send('Não foi possível inserir o cliente.')
   }
 }
 
 const update = async (req, res) => {
-  const {
-    TipoPessoa,
-    Cpf,
-    Cnpj,
-    NomeCliente,
-    RazaoSocial,
-    Cep,
-    Email,
-    Classificacao,
-    TelefoneResidencial,
-    TelefoneComercial,
-  } = req.body
   const { id } = req.params
   try {
     let pool = await sql.connect(config)
-    let cliente = await new Clientes(pool).update(
-      sql.Int,
-      id,
-      TipoPessoa,
-      Cpf,
-      Cnpj,
-      NomeCliente,
-      RazaoSocial,
-      Cep,
-      Email,
-      Classificacao,
-      TelefoneResidencial,
-      TelefoneComercial,
-    )
-    res.status(201).json(cliente)
+    let response = await new Clientes(pool).update(sql.Int, id, req.body)
+    return res.status(200).json(response)
   } catch (error) {
     console.log(error)
+    return res.status(500).send('Não foi possível atualizar o cliente.')
   }
 }
 
--- src/models/Clientes.js
+++ src/models/Clientes.js
@@ -5,52 +5,36 @@ module.exports = class SistemaDAO {
   }
 
   async findAll() {
-    try {
-      let clientes = await this._connection
-        .request()
-        .query('SELECT * from Clientes')
-      return clientes.recordsets[0]
-    } catch (error) {
-      console.log(error)
-    }
+    let clientes = await this._connection
+      .request()
+      .query(`SELECT * FROM  ${this._table}`)
+    return clientes.recordsets[0]
   }
 
   async findById(type, param) {
-    try {
-      let cliente = await this._connection
-        .request()
-        .input('input_parameter', type, param)
-        .query('SELECT * from Clientes where ClienteId = @input_parameter')
-      return cliente.recordsets[0]
-    } catch (error) {
-      console.log(error)
-    }
+    let cliente = await this._connection
+      .request()
+      .input('input_parameter', type, param)
+      .query(`SELECT * FROM ${this._table} WHERE ClienteId = @input_parameter`)
+    return cliente.recordsets[0]
   }
 
   async findByName(param) {
-    try {
-      let cliente = await this._connection
-        .request()
-        .query(`SELECT * from Clientes where NomeCliente LIKE '%${param}%'`)
-      return cliente.recordsets[0]
-    } catch (error) {
-      console.log(error)
-    }
+    let cliente = await this._connection
+      .request()
+      .query(`SELECT * FROM ${this._table} WHERE NomeCliente LIKE '%${param}%'`)
+    return cliente.recordsets[0]
   }
 
   async remove(type, param) {
-    try {
-      let cliente = await this._connection
-        .request()
-        .input('input_parameter', type, param)
-        .query('DELETE Clientes where ClienteId = @input_parameter')
-      return cliente.recordsets[0]
-    } catch (error) {
-      console.log(error)
-    }
+    let ret = await this._connection
+      .request()
+      .input('input_parameter', type, param)
+      .query('DELETE Clientes where ClienteId = @input_parameter')
+    return ret
   }
 
-  async create(
+  async create({
     TipoPessoa,
     Cpf,
     Cnpj,
@@ -61,43 +45,35 @@ module.exports = class SistemaDAO {
     Classificacao,
     TelefoneResidencial,
     TelefoneComercial,
-  ) {
-    try {
-      let cliente = await this._connection
-        .request()
-        .query(
-          `INSERT INTO Clientes(TipoPessoa, Cpf, Cnpj, NomeCliente, RazaoSocial, Cep, Email, Classificacao, TelefoneResidencial, TelefoneComercial) VALUES('${TipoPessoa}','${Cpf}','${Cnpj}','${NomeCliente}','${RazaoSocial}','${Cep}','${Email}','${Classificacao}','${TelefoneResidencial}','${TelefoneComercial}')`,
-        )
-      return cliente.recordsets[0]
-    } catch (error) {
-      console.log(error)
-    }
+  }) {
+    let ret = await this._connection.request().query(
+      `INSERT INTO ${this._table} (TipoPessoa, Cpf, Cnpj, NomeCliente, RazaoSocial, Cep, Email, Classificacao, TelefoneResidencial, TelefoneComercial) 
+          VALUES('${TipoPessoa}','${Cpf}','${Cnpj}','${NomeCliente}','${RazaoSocial}','${Cep}','${Email}','${Classificacao}','${TelefoneResidencial}','${TelefoneComercial}')`,
+    )
+    return ret
   }
 
-  async update(
-    type,
-    ClienteId,
-    TipoPessoa,
-    Cpf,
-    Cnpj,
-    NomeCliente,
-    RazaoSocial,
-    Cep,
-    Email,
-    Classificacao,
-    TelefoneResidencial,
-    TelefoneComercial,
-  ) {
-    try {
-      let cliente = await this._connection
-        .request()
-        .input('input_parameter', type, ClienteId)
-        .query(
-          `UPDATE Clientes SET TipoPessoa='${TipoPessoa}', Cpf='${Cpf}', Cnpj='${Cnpj}', NomeCliente='${NomeCliente}', RazaoSocial='${RazaoSocial}', Cep='${Cep}', Email='${Email}', Classificacao='${Classificacao}', TelefoneResidencial='${TelefoneResidencial}', TelefoneComercial='${TelefoneComercial}' where ClienteId = @input_parameter`,
-        )
-      return cliente.recordsets[0]
-    } catch (error) {
-      console.log(error)
-    }
+  async update(type, ClienteId, cliente) {
+    const {
+      TipoPessoa,
+      Cpf,
+      Cnpj,
+      NomeCliente,
+      RazaoSocial,
+      Cep,
+      Email,
+      Classificacao,
+      TelefoneResidencial,
+      TelefoneComercial,
+    } = cliente
+    let ret = await this._connection
+      .request()
+      .input('input_parameter', type, ClienteId)
+      .query(
+        `UPDATE ${this._table} 
+        SET TipoPessoa='${TipoPessoa}', Cpf='${Cpf}', Cnpj='${Cnpj}', NomeCliente='${NomeCliente}', RazaoSocial='${RazaoSocial}', Cep='${Cep}', Email='${Email}', Classificacao='${Classificacao}', TelefoneResidencial='${TelefoneResidencial}', TelefoneComercial='${TelefoneComercial}' 
+        WHERE ClienteId = @input_parameter`,
+      )
+    return ret
   }
 }
